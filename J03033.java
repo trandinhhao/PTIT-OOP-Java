d7187da6fb39649a8b2d6723174aa16488a9687e
Trần Đình Hào <121801645+trandinhhao@users.noreply.github.com>
2024-08-27 19:32:31 +0700

    Add files via upload

---
+++ J03033.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.math.*;
+
+public class J03033 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- >0) {
+            BigInteger a = sc.nextBigInteger();
+            BigInteger b = sc.nextBigInteger();
+            System.out.println(a.multiply(b).divide(a.gcd(b)));
+        }
+    }
+}
