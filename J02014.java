2f14e88421ed00134b50dd2011359715a431c8f1
Trần Đình Hào <121801645+trandinhhao@users.noreply.github.com>
2024-08-31 18:15:22 +0700

    Add files via upload

---
+++ J02014.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+import java.math.*;
+
+public class J02014 {
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            int tongTrai = 0;
+            int tongPhai = 0;
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+                tongPhai += a[i];
+            }
+            tongPhai -= a[0] + a[1];
+            tongTrai = a[0];
+            int i = 1;
+            boolean check = false;
+            while (i<n-1) {
+                if (tongTrai == tongPhai) {
+                    check = true;
+                    break;
+                }
+                i++;
+                tongTrai+= a[i-1];
+                tongPhai-= a[i];
+            }
+            if (check) {
+                System.out.println(i+1);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
