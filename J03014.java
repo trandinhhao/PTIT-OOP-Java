4ce3014b4abe8df9bd3526a6a425bb7feab433f5
Trần Đình Hào <121801645+trandinhhao@users.noreply.github.com>
2024-09-02 20:06:16 +0700

    Add files via upload

---
+++ J03014.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+import java.math.*;
+
+public class J03014 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+    }
+}
