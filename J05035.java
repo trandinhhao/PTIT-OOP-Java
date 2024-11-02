3e7b19a4597f2135a97bd9461b834b2ff72daa89
Trần Đình Hào <121801645+trandinhhao@users.noreply.github.com>
2024-11-03 01:48:54 +0700

    Add files via upload

---
+++ J05035.java
@@ -0,0 +1,61 @@
+
+import java.util.*;
+
+class SinhVien {
+
+    private int stt;
+    private String msv, ten, lop, email, dn;
+
+    public SinhVien(int stt, String msv, String ten, String lop, String email, String dn) {
+        this.stt = stt;
+        this.msv = msv;
+        this.ten = ten;
+        this.lop = lop;
+        this.email = email;
+        this.dn = dn;
+    }
+
+    public String getMsv() {
+        return msv;
+    }
+
+    public String getDn() {
+        return dn;
+    }
+
+    public String toString() {
+        return stt + " " + msv + " " + ten + " " + lop + " " + email + " " + dn;
+    }
+
+}
+
+public class J05035 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        ArrayList<SinhVien> a = new ArrayList<>();
+        for (int i = 1; i <= t; i++) {
+            SinhVien x = new SinhVien(i, sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine());
+            a.add(x);
+        }
+        Collections.sort(a, new Comparator<SinhVien>() {
+            @Override
+            public int compare(SinhVien o1, SinhVien o2) {
+                return o1.getMsv().compareTo(o2.getMsv());
+            }
+
+        });
+        int q = sc.nextInt();
+        sc.nextLine();
+        while (q-- > 0) {
+            String res = sc.nextLine();
+            for (SinhVien x : a) {
+                if (x.getDn().equals(res)) {
+                    System.out.println(x);
+                }
+            }
+        }
+    }
+}
